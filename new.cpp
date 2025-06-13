3c9c0e364d942ad4dc45eeaa66836118ff70190f
cyber-fanatic <ulhaq7774@gmail.com>
2025-06-14 02:43:36 +0500

    add new.cpp

---
+++ new.cpp
@@ -0,0 +1,21 @@
+/*AdmittedPatient Ap(1, "Josh", 53, 2, "Heat storke", 7000);*/      //(id,name,roomn,stayd,disease,amount)  creating obj using parametrized constructor
+//cout << "\n==================Display Admitted Patient information=============\n";
+//cout<<"Patient id: "<<Ap.get_patientid()<<'\n';
+//cout << "Patient disease: " << Ap.get_disease() << '\n';
+//cout << "Total payable amount after discount: " << Ap.apply_discount(Ap.get_bill_amount())<<" pkr "<<'\n';   //func composition
+
+
+//Demonstrating Multi-level inheritance
+	//creation object of AdmittedPatient class
+
+	//AdmittedPatient Ap(1,"Josh",53,2,"Heat storke",7000);      //(id,name,roomn,stayd,disease,amount)  creating obj using parametrized constructor
+	//cout << "\n==================Display Admitted Patient information=============\n";
+	//cout<<"Patient id: "<<Ap.get_patientid()<<'\n';
+	//cout << "Patient disease: " << Ap.get_disease() << '\n';
+	//cout << "Total payable amount after discount: " << Ap.apply_discount(Ap.get_bill_amount())<<" pkr "<<'\n';   //func composition
+
+
+	//we can also call input() method to prompt user to enter complete data
+	/*AdmittedPatient new_Ap;
+	new_Ap.input();
+	new_Ap.displayInfo();*/
